2fcc6f8423d406a61d485c67f785e5830886fc87
truth <truthadjustr@gmail.com>
2019-07-25 17:14:57 +0800

    Add dynamic, static libraries

---
+++ src/helper.cpp
@@ -0,0 +1,5 @@
+
+int addnum (int a, int b)
+{
+    return a + b;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+int addnum (int,int);
+
 int main(int argc, char *argv[])
 {
     std::cout << "Hello Smeagol\n";
+    std::cout << addnum(3,4) << std::endl;
     return 0;
 }
